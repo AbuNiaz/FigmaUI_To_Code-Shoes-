607f1a9c3f359c86a1a4dfe801658f3315feed1f
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-12 17:53:02 +0600

    Update first_page.dart

--- lib/Shoes Contain/first_page.dart
+++ lib/Shoes Contain/first_page.dart
@@ -19,44 +19,63 @@ class ShoesContent extends StatelessWidget {
                 color: Colors.blueGrey,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(children: const [
-                          Text(
-                            "White",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            "A Pair",
-                            style: TextStyle(color: Colors.black38),
-                          )
-                        ]),
-                        Container(
-                            height: 35,
-                            width: 35,
-                            decoration: const BoxDecoration(
-                                color: Colors.white, shape: BoxShape.circle),
-                            child: const Icon(Icons.check_circle_outline))
-                      ],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(children: const [
+                            Text(
+                              "White",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "A Pair",
+                              style: TextStyle(color: Colors.black38),
+                            )
+                          ]),
+                          Container(
+                              height: 35,
+                              width: 35,
+                              decoration: const BoxDecoration(
+                                  color: Colors.white, shape: BoxShape.circle),
+                              child: const Icon(Icons.check_circle_outline))
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 60,
-                    color: Colors.red,
-                  ),
-                ],
+                    Card(
+                      child: Container(
+                        color: null,
+                        height: 85,
+                        child: Positioned(
+                            top: 0.0,
+                            right: 285.0,
+                            child: GestureDetector(
+                              onTap: () {},
+                              child: const Image(
+                                image: AssetImage('images/shoe2.jpg'),
+                              ),
+                            )),
+                      ),
+                      elevation: 10,
+                    ),
+                    const Spacer(),
+                    const Text(
+                      "220.0 BDT",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
