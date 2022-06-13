286d83233a58a0065bf9a9822f01b7bbac26f391
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-13 23:28:27 +0600

    Update details_home.dart

--- lib/Shoes_details/details_home.dart
+++ lib/Shoes_details/details_home.dart
@@ -6,9 +6,22 @@ class HomeDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
+      body: Container(
+        color: Colors.blueAccent,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: 350,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+            )
+          ],
+        ),
       ),
     );
   }
