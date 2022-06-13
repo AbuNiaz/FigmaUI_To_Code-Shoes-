3eb9d774b168cfd8e9715403547cd3b0225fa0d5
Abu Niaz <niaz35-507@diu.edu.bd>
2022-06-13 22:53:54 +0600

    Create details_home.dart

---
+++ lib/Shoes_details/details_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeDetails extends StatelessWidget {
+  const HomeDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
